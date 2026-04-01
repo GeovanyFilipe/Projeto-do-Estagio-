47da981df5a68a354e26eb9fd4fa1ae8f533bbf1
Emanue da Costa Rabaçal <emanuelcostoado2gmail.com>
2026-04-01 15:17:01 +0100

    Alterações no componente download

--- src/app/paginas/download/download.component.ts
+++ src/app/paginas/download/download.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from '../../layout/menu/menu.component';
 
 @Component({
   selector: 'app-download',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MenuComponent ],
   templateUrl: './download.component.html',
   styleUrl: './download.component.css'
 })
