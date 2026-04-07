aa54e80631e6e74c6c0e2c7f54c78383c0e39f4e
Emanue da Costa Rabaçal <emanuelcostoado2gmail.com>
2026-04-07 13:56:53 +0100

    Problema do app.config.server.ts resolvido

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,8 @@
-import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(withRoutes(serverRoutes))]
+  providers: []
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
