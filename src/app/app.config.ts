b1ed2478ff648fdb1affac55533916c93b4efd83
Emanue da Costa Rabaçal <emanuelcostoado2gmail.com>
2026-04-17 14:09:34 +0100

    Melhorei o botão de entrar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,17 +5,28 @@ import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+// Firebase imports
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'top',
-        anchorScrolling: 'enabled'
-      })
-    ),
-    provideHttpClient(),
-    provideClientHydration(withEventReplay())
-  ]
+providers: [
+  provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(
+    routes,
+    withInMemoryScrolling({
+      scrollPositionRestoration: 'top',
+      anchorScrolling: 'enabled'
+    })
+  ),
+  provideHttpClient(),
+  provideClientHydration(withEventReplay()),
+
+  // Firebase
+  provideFirebaseApp(() => initializeApp(environment.firebase)),
+  provideAnalytics(() => getAnalytics()),
+  ScreenTrackingService,
+  UserTrackingService
+]
 };
